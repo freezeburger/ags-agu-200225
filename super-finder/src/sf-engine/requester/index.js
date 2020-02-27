420bac4387c9c399e9b3bc9d1af64accfa36a5ea
unknown <renaud.dubuis@decryptage.net>
2020-02-27 09:34:13 +0100

    Add Catch Promise

--- super-finder/src/sf-engine/requester/index.js
+++ super-finder/src/sf-engine/requester/index.js
@@ -1,9 +1,9 @@
 const got = require('got');
 const gotGoogle = got.extend({
-    prefixUrl: 'https://search.lycos.fr/web/?q='//,'http://google.com/search?q'
+    prefixUrl: 'https://search.lycoss.fr/web/?q='//,'http://google.com/search?q'
 });
 
 module.exports.request = search => gotGoogle(search).then( response => {
     //console.log(response)
     return response.body.replace(/<script.*>.*<\/script>/ims,'');
-}) 
+}).catch(err => console.log(1234,err))
