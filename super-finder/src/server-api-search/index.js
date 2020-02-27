c7609c04054d6f6043a0ea9d64353a52292b4057
unknown <renaud.dubuis@decryptage.net>
2020-02-27 11:32:35 +0100

    Koa Routing and Cors

--- super-finder/src/server-api-search/index.js
+++ super-finder/src/server-api-search/index.js
@@ -7,14 +7,30 @@ module.title = "Server Api Search";
  */
 
 const Koa = require("koa");
+const cors = require("@koa/cors");
+const Router = require("@koa/router");
+const bodyParser = require("koa-bodyparser");
 const koaServer = new Koa();
+const koaRouter = new Router();
 
-const requestHandler = async (ctx) => {
-  console.log("Incoming Request");
+koaRouter.get('/', (ctx, next) => {
   ctx.body = module.title;
+});
+
+koaRouter.get('/search', (ctx, next) => {
+  ctx.body = 'Search';
+  console.log(ctx.request.body)
+});
+
+const generalHandler = async (ctx,next) => {
+  console.log("Incoming Request");
+  next()
 };
 
-koaServer.use(requestHandler);
+koaServer.use(cors());
+koaServer.use(bodyParser());
+koaServer.use(generalHandler);
+koaServer.use(koaRouter.routes());
 
 const httpServer = createHttpServer(koaServer.callback());
 
